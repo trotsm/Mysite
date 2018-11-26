33986003a4130ebf63c935d84966fe96931cbc45
Mykola Trots <trots86@gmail.com>
2018-11-26 21:39:32 +0200

    Good design of site

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mainApp',
     'news',
+    'waterfowl'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Mysite/urls.py
+++ Mysite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
     path('news/', include('news.urls')),
+    path('waterfowl/', include('waterfowl.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
