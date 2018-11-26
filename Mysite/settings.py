b6e276a933be6c645aed88562f6c06491fdeb93f
Mykola Trots <trots86@gmail.com>
2018-11-26 23:11:00 +0200

    Good design of site 2

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mainApp',
     'news',
-    'waterfowl'
+    'waterfowl',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
