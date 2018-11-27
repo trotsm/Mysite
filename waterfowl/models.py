bf980abea82c1cd35221fb46689d671e6b43c34b
Mykola Trots <trots86@gmail.com>
2018-11-27 14:08:02 +0200

    Good design of site 5

---
+++ waterfowl/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Articles(models.Model):
+    title = models.CharField(max_length=120)
+    post = models.TextField()
+    date = models.DateTimeField()
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
